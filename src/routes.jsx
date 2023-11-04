962b06c5ce6e37c5153379ca7df94d90d59d39ca
ceciliaramoscavazos <aceciliaramos@gmail.com>
2023-11-03 20:29:18 -0600

    change name

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,4 @@
 import * as Home from './pages/home/route.jsx';
-import * as Posts from './pages/posts/route.jsx';
+import * as Notes from './pages/notes/route.jsx';
 
-export default [Home, Posts];
+export default [Home, Notes];
